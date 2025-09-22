88e4f4b3576d8fa7e90fab4540ed43a90eb84e37
Alexis <prietoalexis99@gmail.com>
2025-09-22 15:20:13 -0300

    ajustes boton Agregar Usuario

--- src/app/dashboard/usuarios/page.tsx
+++ src/app/dashboard/usuarios/page.tsx
@@ -76,10 +76,7 @@ export default function UsuariosPage() {
   return (
     <Box className={styles.usuariosPageContainer}>
       <CustomButton
-        variant="contained"
-        color="primary"
         onClick={() => handleOpenModal()}
-        sx={{ mb: 2 }}
         width="20%"
       >
         Crear usuario
