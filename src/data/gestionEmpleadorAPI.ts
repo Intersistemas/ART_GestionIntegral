fb87118995102f2796423fd7eaef48142f73bdab
Rodrigo J López <rodrilopez90@gmail.com>
2025-10-02 13:40:41 -0300

    Valores por defecto y token

--- src/data/gestionEmpleadorAPI.ts
+++ src/data/gestionEmpleadorAPI.ts
@@ -4,6 +4,7 @@ import { ExternalAPI } from "./api";
 import Personal, { Parameters as GetPersonalParams} from "@/app/inicio/empleador/cobertura/types/persona";
 import dayjs from "dayjs";
 import { stringifyValues } from "@/utils/utils";
+import { getSession } from "next-auth/react";
 
 const getCurrentPeriodo = (): number => Number(dayjs().format('YYYYMM'));
 
@@ -11,13 +12,19 @@ export class GestionEmpleadorAPIClass extends ExternalAPI {
   readonly basePath = "http://arttest.intersistemas.ar:8670"; ///ToDo: debo agregarlo al env.
   //#region Personal
   readonly getPersonalPath = "/api/AfiliadoCuentaCorriente/";
-  // getPersonal = async ({ cuit: c, periodo: p = getCurrentPeriodo(), page = "1,1", ...rest }: GetPersonalParams = {}
-  getPersonal = async (params: GetPersonalParams = {}
-  ) => axios.get<Personal[]>(
-    this.getURL({ path: this.getPersonalPath, search: stringifyValues(params) }).toString(),
-  ).then(({ data }) => data);
+  private getPersonalToken = "";
+  getPersonal = async (params: GetPersonalParams = {}) => {
+    params.periodo ??= getCurrentPeriodo();
+    params.page ??= "1,1";
+    const token = (await getSession())?.accessToken ?? "";
+    if (token !== this.getPersonalToken) this.getPersonalToken = token;
+    return axios.get<Personal[]>(
+      this.getURL({ path: this.getPersonalPath, search: stringifyValues(params) }).toString(),
+      { headers: { Authorization: `Bearer ${token}` } }
+    ).then(({ data }) => data);
+  }
   useGetPersonal = (params: GetPersonalParams = {}) => useSWR(
-    [this.basePath, this.getPersonalPath, JSON.stringify(params)], () => this.getPersonal(params)
+    [this.basePath, this.getPersonalPath, this.getPersonalToken, JSON.stringify(params)], () => this.getPersonal(params)
   );
   //#endregion
 }
