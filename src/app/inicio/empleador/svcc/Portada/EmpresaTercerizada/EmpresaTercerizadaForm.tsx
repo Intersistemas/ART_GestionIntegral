3ccf3901a94f3811fe44d3e45d372a72bf23ac25
Rodrigo J López <rodrilopez90@gmail.com>
2025-12-01 16:00:12 -0300

    Simplificación de implementación de mantenimiento de empresa tercerizada

--- src/app/inicio/empleador/svcc/Portada/EmpresaTercerizada/EmpresaTercerizadaForm.tsx
+++ src/app/inicio/empleador/svcc/Portada/EmpresaTercerizada/EmpresaTercerizadaForm.tsx
@@ -1,205 +1,123 @@
 import { EmpresaTercerizadaDTO } from "@/data/gestionEmpleadorAPI";
-import CustomModal from "@/utils/ui/form/CustomModal";
-import { Dispatch, SetStateAction, useCallback, useState } from "react";
-import { Grid, IconButton, InputAdornment, TextField, Tooltip, Typography } from "@mui/material";
-import CustomButton from "@/utils/ui/button/CustomButton";
+import { Grid, IconButton, InputAdornment, TextField, Tooltip } from "@mui/material";
 import { MoreHoriz } from "@mui/icons-material";
 import Formato from "@/utils/Formato";
-
-export type EmpresaTercerizadaDataErrors = Partial<Record<keyof EmpresaTercerizadaDTO, string>>;
-export type EmpresaTercerizadaCommonFormErrors = {
-  message?: string;
-  data: EmpresaTercerizadaDataErrors;
-};
-export type EmpresaTercerizadaCommonFormProps = {
-  disabled?: Partial<Record<keyof EmpresaTercerizadaDTO, boolean>>;
-}
-export type EmpresaTercerizadaFormProps = EmpresaTercerizadaCommonFormProps & {
-  data: Partial<EmpresaTercerizadaDTO>;
-  errors?: EmpresaTercerizadaDataErrors;
-  onChange?: (changes: Partial<EmpresaTercerizadaDTO>) => void;
-};
-export type EmpresaTercerizadaModalFormWorkingState = {
-  isWorking: boolean;
-  message?: string;
-}
-export type OnConfirmCallbackInfo = {
-  data: {
-    state: Partial<EmpresaTercerizadaDTO>,
-    setState: Dispatch<SetStateAction<Partial<EmpresaTercerizadaDTO>>>
-  };
-  working: {
-    state: EmpresaTercerizadaModalFormWorkingState,
-    setState: Dispatch<SetStateAction<EmpresaTercerizadaModalFormWorkingState>>
-  };
-  errors: {
-    state: EmpresaTercerizadaCommonFormErrors,
-    setState: Dispatch<SetStateAction<EmpresaTercerizadaCommonFormErrors>>
-  };
-}
-export type EmpresaTercerizadaModalFormProps = EmpresaTercerizadaCommonFormProps & {
-  action?: "create" | "read" | "update" | "delete";
-  size?: "small" | "large" | "mid";
-  title?: string;
-  init?: Partial<EmpresaTercerizadaDTO>;
-  errors?: EmpresaTercerizadaCommonFormErrors;
-  onConfirm?: (info: OnConfirmCallbackInfo) => void;
-  onCancel?: (data: Partial<EmpresaTercerizadaDTO>) => void;
-};
+import { Form } from "@/utils/ui/form/Form";
 
 const tooltip_SlotProps = { tooltip: { sx: { fontSize: "1.2rem", fontWeight: 500 } } };
-
-export function EmpresaTercerizadaForm({
-  data, disabled, errors, onChange = () => {}
-}: EmpresaTercerizadaFormProps) {
-  return (
-    <Grid container size={12} spacing={2}>
-      <Grid size={3}>
-        <TextField
-          name="idEstablecimientoEmpresa"
-          type="number"
-          label="Establ. Empresa"
-          value={data.idEstablecimientoEmpresa}
-          disabled={disabled?.idEstablecimientoEmpresa}
-          onChange={({ target: { value }}) => onChange({ idEstablecimientoEmpresa: Number(value) })}
-          error={!!errors?.idEstablecimientoEmpresa}
-          helperText={errors?.idEstablecimientoEmpresa}
-          slotProps={{
-            input: {
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Tooltip
-                    title="Buscar Establ. Empresa"
-                    arrow
-                    slotProps={tooltip_SlotProps}
-                  >
-                    <IconButton
-                      color="primary"
-                      size="large"
-                      disabled={disabled?.idEstablecimientoEmpresa}
-                      // onClick={() => onLookup()}
-                    >
-                      <MoreHoriz />
-                    </IconButton>
-                  </Tooltip>
-                </InputAdornment>
-              ),
-            }
-          }}
-          fullWidth
-        />
-      </Grid>
-      <Grid size={9}>
-        <TextField
-          name="Placeholder"
-          label="Establ. Empresa - Descripcion"
-          disabled
-          fullWidth
-        />
-      </Grid>
-      <Grid size={4}>
-        <TextField
-          name="cuit"
-          label="CUIT"
-          placeholder="Ingrese CUIT (11 dígitos)"
-          value={Formato.CUIP(data.cuit)}
-          onChange={({ target: { value }}) => onChange({ cuit: Number((value || '').replace(/[^0-9]/g, ''))})}
-          error={!!errors?.cuit}
-          helperText={errors?.cuit}
-          disabled={disabled?.cuit}
-          fullWidth
-        />
-      </Grid>
-      <Grid size={4}>
-        <TextField
-          name="ciiu"
-          label="CIIU"
-          value={data.ciiu}
-          onChange={({ target: { value }}) => onChange({ ciiu: Number(value)})}
-          error={!!errors?.ciiu}
-          helperText={errors?.ciiu}
-          fullWidth
-          disabled={disabled?.ciiu}
-          placeholder="CIIU"
-        />
-      </Grid>
-      <Grid size={4}>
-        <TextField
-          name="cantidadTrabajadores"
-          label="Cant. trabajadores"
-          value={data.cantidadTrabajadores}
-          onChange={({ target: { value }}) => onChange({ cantidadTrabajadores: Number(value)})}
-          error={!!errors?.cantidadTrabajadores}
-          helperText={errors?.cantidadTrabajadores}
-          fullWidth
-          disabled={disabled?.cantidadTrabajadores}
-          placeholder="Cant. trabajadores"
-        />
-      </Grid>
-    </Grid>
-  );
-}
-const allDisabled: Partial<Record<keyof EmpresaTercerizadaDTO, boolean>> = {
-  interno: true,
-  idEstablecimientoEmpresa: true,
-  cuit: true,
-  ciiu: true,
-  cantidadTrabajadores: true,
-};
-export default function EmpresaTercerizadaModalForm({
-  action, title, size, init, disabled, errors: initErrors, onCancel = () => {}, onConfirm = () => {}
-}: EmpresaTercerizadaModalFormProps) {
-  const [data, setData] = useState<Partial<EmpresaTercerizadaDTO>>(JSON.parse(JSON.stringify({ ...init })));
-  const [working, setWorking] = useState<EmpresaTercerizadaModalFormWorkingState>({ isWorking: false });
-  const [errors, setErrors] = useState<EmpresaTercerizadaCommonFormErrors>({ ...initErrors, data: { ...initErrors?.data } });
-  const onChange = useCallback((changes: Partial<EmpresaTercerizadaDTO>) => setData((o) => ({...o, ...changes})), []);
-
-  return (
-    <CustomModal
-      open={!!action}
-      onClose={handleOnClose}
-      title={title}
-      size={size}
-    >
-      <Grid container spacing={2} justifyContent="center">
-        {errors.message && <Typography variant="h5" color="var(--naranja)" textAlign="center">{errors.message}</Typography>}
-        <Grid container flexDirection="column" justifyContent="space-between" minHeight="500px">
-          <EmpresaTercerizadaForm
-            data={data}
-            disabled={{ ...(action === "read" ? allDisabled : {}), ...disabled }}
-            errors={errors.data}
-            onChange={onChange}
-          />
-          <Grid container flexDirection="column" justifyContent="space-between">
-            <Grid borderTop="1px solid #e9ecef" />
-            <Grid container>
-              {action !== "read" &&
-                <CustomButton
-                  onClick={handleOnConfirm}
-                  disabled={working.isWorking}
+export const EmpresaTercerizadaForm: Form<EmpresaTercerizadaDTO> = ({
+  data,
+  disabled = {},
+  errors = {},
+  helpers = {},
+  onChange = () => { }
+}) => (
+  <Grid container size={12} spacing={2} maxHeight="fit-content">
+    <Grid size={3}>
+      <TextField
+        name="idEstablecimientoEmpresa"
+        type="number"
+        label="Establ. Empresa"
+        value={data.idEstablecimientoEmpresa}
+        disabled={disabled.idEstablecimientoEmpresa}
+        onChange={({ target: { value } }) => onChange({ idEstablecimientoEmpresa: Number(value) })}
+        error={errors.idEstablecimientoEmpresa}
+        helperText={helpers.idEstablecimientoEmpresa}
+        slotProps={{
+          input: {
+            endAdornment: (
+              <InputAdornment position="end">
+                <Tooltip
+                  title="Buscar Establ. Empresa"
+                  arrow
+                  slotProps={tooltip_SlotProps}
                 >
-                  {working.message || (action === "delete" ? "Borrar" : "Guardar")}
-                </CustomButton>
-              }
-              <CustomButton
-                onClick={handleOnClose}
-                color="secondary"
-                disabled={working.isWorking}
-              >
-                {action === "read" ? "Cerrar" : "Cancelar"}
-              </CustomButton>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </CustomModal>
-  );
-  function handleOnClose() { onCancel(data); }
-  function handleOnConfirm() {
-    onConfirm({
-      data: { state: data, setState: setData },
-      working: { state: working, setState: setWorking },
-      errors: { state: errors, setState: setErrors },
-    });
-  }
-}
+                  <IconButton
+                    color="primary"
+                    size="large"
+                    disabled={disabled.idEstablecimientoEmpresa}
+                  // onClick={() => onLookup()}
+                  >
+                    <MoreHoriz />
+                  </IconButton>
+                </Tooltip>
+              </InputAdornment>
+            ),
+          }
+        }}
+        fullWidth
+      />
+    </Grid>
+    <Grid size={9}>
+      <TextField
+        name="Placeholder"
+        label="Establ. Empresa - Descripcion"
+        disabled
+        fullWidth
+      />
+    </Grid>
+    <Grid size={4}>
+      <TextField
+        name="cuit"
+        label="CUIT"
+        placeholder="Ingrese CUIT (11 dígitos)"
+        value={Formato.CUIP(data.cuit)}
+        disabled={disabled.cuit}
+        onChange={({ target: { value } }) => onChange({ cuit: value ? Number((value || '').replace(/[^0-9]/g, '')) : undefined })}
+        error={errors.cuit}
+        helperText={helpers.cuit}
+        fullWidth
+      />
+    </Grid>
+    <Grid size={4}>
+      <TextField
+        name="ciiu"
+        type="number"
+        label="CIUO"
+        value={data.ciiu}
+        disabled={disabled.ciiu}
+        onChange={({ target: { value } }) => onChange({ ciiu: Number(value) })}
+        error={errors.ciiu}
+        helperText={helpers.ciiu}
+        slotProps={{
+          input: {
+            endAdornment: (
+              <InputAdornment position="end">
+                <Tooltip
+                  title="Buscar CIIU"
+                  arrow
+                  slotProps={tooltip_SlotProps}
+                >
+                  <IconButton
+                    color="primary"
+                    size="large"
+                    disabled={disabled.ciiu}
+                  // onClick={() => onLookup()}
+                  >
+                    <MoreHoriz />
+                  </IconButton>
+                </Tooltip>
+              </InputAdornment>
+            ),
+          }
+        }}
+        fullWidth
+      />
+    </Grid>
+    <Grid size={4}>
+      <TextField
+        name="cantidadTrabajadores"
+        type="number"
+        label="Cant. trabajadores"
+        value={data.cantidadTrabajadores}
+        disabled={disabled.cantidadTrabajadores}
+        onChange={({ target: { value } }) => onChange({ cantidadTrabajadores: value ? Number(value) : undefined })}
+        error={errors.cantidadTrabajadores}
+        helperText={helpers.cantidadTrabajadores}
+        fullWidth
+      />
+    </Grid>
+  </Grid>
+);
+
+export default EmpresaTercerizadaForm;
--- src/app/inicio/empleador/svcc/Portada/EmpresaTercerizada/EmpresaTercerizadaHandler.tsx
+++ src/app/inicio/empleador/svcc/Portada/EmpresaTercerizada/EmpresaTercerizadaHandler.tsx
@@ -1,11 +1,15 @@
 import { ReactNode, useState } from "react";
-import EmpresaTercerizadaBrowse from "./EmpresaTercerizadaBrowse";
-import { useSVCCPresentacionContext } from "../../context";
+import { Data } from "@/utils/ui/table/Browse";
 import gestionEmpleadorAPI, {
-  Pagination, EmpresaTercerizadaDTO, SVCCEmpresaTercerizadaUpdateParams, SVCCEmpresaTercerizadaDeleteParams
+  EmpresaTercerizadaDTO, SVCCEmpresaTercerizadaUpdateParams, SVCCEmpresaTercerizadaDeleteParams
 } from "@/data/gestionEmpleadorAPI";
-
-import EmpresaTercerizadaModalForm, { OnConfirmCallbackInfo } from "./EmpresaTercerizadaForm";
+import { useAuth } from "@/data/AuthContext";
+import CustomModal from "@/utils/ui/form/CustomModal";
+import { Grid, Typography } from "@mui/material";
+import CustomButton from "@/utils/ui/button/CustomButton";
+import { useSVCCPresentacionContext } from "../../context";
+import EmpresaTercerizadaBrowse from "./EmpresaTercerizadaBrowse";
+import EmpresaTercerizadaForm from "./EmpresaTercerizadaForm";
 
 const {
   useSVCCEmpresaTercerizadaList,
@@ -14,139 +18,170 @@ const {
   useSVCCEmpresaTercerizadaDelete,
 } = gestionEmpleadorAPI;
 
-type State = {
-  modal?: ReactNode
-}
+type EditAction = "create" | "read" | "update" | "delete";
+type EditState = {
+  action?: EditAction,
+  data?: Partial<EmpresaTercerizadaDTO>,
+  disabled?: Partial<Record<keyof EmpresaTercerizadaDTO, boolean>>,
+  errors?: Partial<Record<keyof EmpresaTercerizadaDTO, boolean>>,
+  helpers?: Partial<Record<keyof EmpresaTercerizadaDTO, ReactNode>>,
+  message?: string;
+};
 export default function EmpresaTercerizadaHandler() {
-  const [state, setState] = useState<State>({});
-  const { ultima: { data: presentacion } } = useSVCCPresentacionContext();
+  const [edit, setEdit] = useState<EditState>({});
+  const { ultima: { data: presentacion }, } = useSVCCPresentacionContext();
   const [{ index, size }, setPage] = useState({ index: 0, size: 100 });
-  const [data, setData] = useState<Pagination<EmpresaTercerizadaDTO>>({ index: index + 1, size, count: 0, pages: 0, data: [] });
-  const { isLoading, mutate } = useSVCCEmpresaTercerizadaList(
+  const [data, setData] = useState<Data<EmpresaTercerizadaDTO>>({ index, size, count: 0, pages: 0, data: [] });
+  const { user } = useAuth();
+  const { isLoading, isValidating, mutate } = useSVCCEmpresaTercerizadaList(
     { page: `${index + 1},${size}` },
     {
       revalidateOnFocus: false,
       onSuccess(data) { setData({ ...data, index: data.index - 1 }) },
     }
   );
-  const { trigger: triggerCreate } = useSVCCEmpresaTercerizadaCreate({ onSuccess() { mutate(); }});
-
+  const { trigger: triggerCreate, isMutating: isCreating } = useSVCCEmpresaTercerizadaCreate({ onSuccess() { mutate(); } });
   const [updateParams, setUpdateParams] = useState<SVCCEmpresaTercerizadaUpdateParams | undefined>();
-  const { trigger: triggerUpdate } = useSVCCEmpresaTercerizadaUpdate(updateParams, { onSuccess() { mutate(); }});
-
+  const { trigger: triggerUpdate, isMutating: isUpdating } = useSVCCEmpresaTercerizadaUpdate(updateParams, { onSuccess() { mutate(); } });
   const [deleteParams, setDeleteParams] = useState<SVCCEmpresaTercerizadaDeleteParams | undefined>();
-  const { trigger: triggerDelete } = useSVCCEmpresaTercerizadaDelete(deleteParams, { onSuccess() { mutate(); }});
+  const { trigger: triggerDelete, isMutating: isDeleting } = useSVCCEmpresaTercerizadaDelete(deleteParams, { onSuccess() { mutate(); } });
+  const isWorking = isCreating || isUpdating || isDeleting || isLoading || isValidating;
+
   const readonly = presentacion?.presentacionFecha != null;
   return (
     <>
       <EmpresaTercerizadaBrowse
-        isLoading={isLoading}
+        cuit={user?.cuit ?? 0}
+        isLoading={isLoading || isValidating}
         data={data}
         onPageIndexChange={(index: number) => setPage((o) => ({ ...o, index }))}
         onPageSizeChange={(size: number) => setPage((o) => ({ ...o, size }))}
         onCreate={readonly ? undefined : () => onAction("create")}
-        onRead={(data) => onAction("read", data)}
-        onUpdate={readonly ? undefined : (data) => onAction("update", data)}
-        onDelete={readonly ? undefined : (data) => onAction("delete", data)}
+        onRead={(data) => data.interno ? () => onAction("read", data) : undefined}
+        onUpdate={readonly ? undefined : (data) => data.interno ? () => onAction("update", data) : undefined}
+        onDelete={readonly ? undefined : (data) => data.interno ? () => onAction("delete", data) : undefined}
       />
-      {state.modal}
+      <CustomModal
+        open={!!edit.action}
+        onClose={handleEditOnClose}
+        title={editTitle()}
+        size="large"
+        actions={(
+          <Grid container spacing={2}>
+            {edit.action !== "read" &&
+              <CustomButton
+                onClick={handleEditOnConfirm}
+                disabled={isWorking}
+              >
+                {actionMessage() || (edit.action === "delete" ? "Borrar" : "Guardar")}
+              </CustomButton>
+            }
+            <CustomButton
+              onClick={handleEditOnClose}
+              color="secondary"
+              disabled={isWorking}
+            >
+              {edit.action === "read" ? "Cerrar" : "Cancelar"}
+            </CustomButton>
+          </Grid>
+        )}
+      >
+        <Grid container spacing={2} justifyContent="center" minHeight="500px">
+          {edit.message && <Typography variant="h5" color="var(--naranja)" textAlign="center">{edit.message}</Typography>}
+          <EmpresaTercerizadaForm
+            data={edit?.data ?? {}}
+            disabled={edit?.disabled}
+            errors={edit?.errors}
+            helpers={edit?.helpers}
+            onChange={handleOnChange}
+          />
+        </Grid>
+      </CustomModal>
     </>
   );
-  function onAction(action: "create" | "read" | "update" | "delete", data?: EmpresaTercerizadaDTO) {
-    switch (action) {
+  function actionMessage() {
+    if (isLoading || isValidating) return "Cargando...";
+    if (isCreating) return "Agregando...";
+    if (isUpdating) return "Modificando...";
+    if (isDeleting) return "Borrando...";
+  }
+  function editTitle() {
+    const value = "Empresa Tercerizada";
+    switch (edit.action) {
+      case "create": return `Agregando ${value}`;
+      case "read": return `Consultando ${value}`;
+      case "update": return `Modificando ${value}`;
+      case "delete": return `Borrando ${value}`;
+    }
+  }
+  function handleOnChange(changes: Partial<EmpresaTercerizadaDTO>) {
+    setEdit((o) => ({ ...o, data: { ...o.data, ...changes } }));
+  }
+  function handleEditOnClose() { setEdit({}); }
+  function handleEditOnConfirm() {
+    switch (edit.action) {
       case "create": {
-        break;
-      }
-      case "read": {
+        triggerCreate(edit.data as EmpresaTercerizadaDTO)
+          .then((data) => {
+            console.info(data);
+            handleEditOnClose();
+          }, (error) => {
+            console.error(error);
+            setEdit((o) => ({ ...o, message: "Ocurrió un error creando establecimiento declarado" }));
+          });
         break;
       }
       case "update": {
-        if (data?.interno == null) {
-          action = "read"
-        } else {
-          setUpdateParams({ id: data?.interno });
-        }
+        triggerUpdate(edit.data as EmpresaTercerizadaDTO)
+          .then((data) => {
+            console.info(data);
+            handleEditOnClose();
+          }, (error) => {
+            console.error(error);
+            setEdit((o) => ({ ...o, message: "Ocurrió un error actualizando establecimiento declarado" }));
+          });
         break;
       }
       case "delete": {
-        if (data?.interno == null) {
-          action = "read"
-        } else {
-          setDeleteParams({ id: data?.interno });
-        }
+        triggerDelete(edit.data as EmpresaTercerizadaDTO)
+          .then((data) => {
+            console.info(data);
+            handleEditOnClose();
+          }, (error) => {
+            console.error(error);
+            setEdit((o) => ({ ...o, message: "Ocurrió un error borrando establecimiento declarado" }));
+          });
+        break;
+      }
+      case "read": {
+        handleEditOnClose();
         break;
       }
     }
-    setState((o) => ({
-      ...o,
-      modal: (
-        <EmpresaTercerizadaModalForm
-          action={action}
-          init={data}
-          title={getTitle()}
-          onCancel={handleOnClose}
-          onConfirm={handleOnConfirm}
-        />
-      )
-    }));
-    function getTitle() {
-      const registro = "Empresa Tercerizada";
-      switch (action){
-        case "create": return `Agregando ${registro}`;
-        case "update": return `Actualizando ${registro}`;
-        case "delete": return `Borrando ${registro}`;
-        case "read": return `Consultando ${registro}`;
+  }
+  function onAction(action: EditAction, data?: EmpresaTercerizadaDTO) {
+    switch (action) {
+      case "update": {
+        setUpdateParams({ id: data!.interno! });
+        break;
+      }
+      case "delete": {
+        setDeleteParams({ id: data!.interno! });
+        break;
       }
     }
-    function handleOnClose() {
-      setState((o) => ({ ...o, modal: undefined }));
-    }
-    function handleOnConfirm(info: OnConfirmCallbackInfo) {
-      switch (action){
-        case "create": {
-          info.working.setState({ isWorking: true, message: "Creando.." });
-          triggerCreate(info.data.state)
-            .then((data) => {
-              console.info(data);
-              handleOnClose();
-            }, (error) => {
-              console.error(error);
-              info.working.setState({ isWorking: false });
-              info.errors.setState((o) => ({ ...o, message: "Ocurrió un error creando empresa tercerizada" }));
-            });
-          break;
+    setEdit({
+      action,
+      data: data ? JSON.parse(JSON.stringify(data)) : {},
+      disabled: ["read", "delete"].includes(action)
+        ? {
+          interno: true,
+          idEstablecimientoEmpresa: true,
+          cuit: true,
+          ciiu: true,
+          cantidadTrabajadores: true,
         }
-        case "update": {
-          info.working.setState({ isWorking: true, message: "Actualizando.." });
-          triggerUpdate(info.data.state)
-            .then((data) => {
-              console.info(data);
-              handleOnClose();
-            }, (error) => {
-              console.error(error);
-              info.working.setState({ isWorking: false });
-              info.errors.setState((o) => ({ ...o, message: "Ocurrió un error actualizando empresa tercerizada" }));
-            });
-          break;
-        }
-        case "delete": {
-          info.working.setState({ isWorking: true, message: "Borrando.." });
-          triggerDelete(info.data.state)
-            .then((data) => {
-              console.info(data);
-              handleOnClose();
-            }, (error) => {
-              console.error(error);
-              info.working.setState({ isWorking: false });
-              info.errors.setState((o) => ({ ...o, message: "Ocurrió un error borrando empresa tercerizada" }));
-            });
-          break;
-        }
-        case "read": {
-          handleOnClose();
-          break;
-        }
-      }
-    }
+        : {},
+    });
   }
 }
