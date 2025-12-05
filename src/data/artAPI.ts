160368b969a8225a8dc901714495c17f9d4bc60c
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-12-05 16:10:44 -0300

    Ajustes en los formularios

--- src/data/artAPI.ts
+++ src/data/artAPI.ts
@@ -21,6 +21,49 @@ export class ArtAPIClass extends ExternalAPI {
   );
   //#endregion
 
+  //#region Establecimientos
+  readonly getEstablecimientosURL = (params: { CUIT?: number | string } = {}) => {
+    // params.CUIT ??= useAuth().user?.empresaCUIT ?? 0; // se pasa desde el componente
+    const cuit = params.CUIT ?? 0;
+    return this.getURL({ path: `/api/Establecimientos/Empresa/${cuit}` }).toString();
+  };
+  getEstablecimientos = async (params: { CUIT?: number | string } = {}) => tokenizable.get(
+    this.getEstablecimientosURL(params),
+  ).then(({ data }) => data);
+  useGetEstablecimientos = (params: { CUIT?: number | string } = {}) => useSWR(
+    [this.getEstablecimientosURL(params), token.getToken()], () => this.getEstablecimientos(params),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
+  
+
+
+    readonly getEstablecimientoByIdURL = (id: number) => {
+    return this.getURL({ path: `/api/Establecimientos/${id}` }).toString();
+  };
+  getEstablecimientoById = async (id: number) => tokenizable.get(
+    this.getEstablecimientoByIdURL(id),
+  ).then(({ data }) => data);
+  useGetEstablecimientoById = (id?: number) => useSWR(
+    id && token.getToken() ? [this.getEstablecimientoByIdURL(id), token.getToken()] : null, () => this.getEstablecimientoById(id as number),
+     {
+
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+
+    }     
+  );
+
+
+  
+
+
+
+  //#endregion
+
   //#region FormulariosRAR
   readonly getFormulariosRARURL = (params: ParametersFormularioRar = {}) => {
     //params.CUIT ??= useAuth().user?.empresaCUIT ?? 0; este parametro lo paso desde el componente que lo usa
@@ -40,7 +83,42 @@ export class ArtAPIClass extends ExternalAPI {
       // Si quieres que la clave no dispare fetch hasta que exista token, puedes usar: (token.getToken() ? key : null)
     }     
   );
+
+ //  Formulario RAR por interno (/api/FormulariosRAR/{id})
+ readonly getFormularioRARByIdURL = (id: number) => this.getURL({ path: `/api/FormulariosRAR/${id}` }).toString();
+ getFormularioRARById = async (id: number) => tokenizable.get(
+   this.getFormularioRARByIdURL(id),
+ ).then(({ data }) => data);
+ useGetFormularioRARById = (id?: number) => useSWR(
+   id && token.getToken() ? [this.getFormularioRARByIdURL(id), token.getToken()] : null,
+   () => this.getFormularioRARById(id as number),
+   {
+     revalidateOnFocus: false,
+     revalidateOnReconnect: false,
+   }
+ );
   //#endregion
+
+    //#region Empresas por CUIT
+  readonly getEmpresaByCUITURL = (params: { CUIT?: number | string } = {}) => {
+    return this.getURL({ path: "/api/Empresas/CUIT", search: toURLSearch(params) }).toString();
+  };
+
+  getEmpresaByCUIT = async (params: { CUIT?: number | string } = {}) =>
+    tokenizable.get(this.getEmpresaByCUITURL(params))
+      .then(({ data }) => data);
+
+  useGetEmpresaByCUIT = (params: { CUIT?: number | string } = {}) => useSWR(
+    [this.getEmpresaByCUITURL(params), token.getToken()],
+    () => this.getEmpresaByCUIT(params),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+  //#endregion
+
+
 }
 
 const ArtAPI = Object.seal(new ArtAPIClass());
