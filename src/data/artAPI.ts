05f9d939143ffce31852a651f023e30a5f01c2a5
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-12-09 09:31:43 -0300

    Type

--- src/data/artAPI.ts
+++ src/data/artAPI.ts
@@ -2,14 +2,14 @@ import useSWR from "swr";
 import { ExternalAPI } from "./api";
 import { token } from "./usuarioAPI";
 import RefEmpleador from "@/app/inicio/usuarios/interfaces/RefEmpleador";
-import FormularioRAR, { ParametersFormularioRar } from "@/app/inicio/empleador/formularioRAR/types/TformularioRar";
+import FormularioRAR, { ParametersFormularioRar, ParametersEmpresaByCUIT, EstablecimientoById, ParametersEstablecimientoByCUIT } from "@/app/inicio/empleador/formularioRAR/types/TformularioRar";
 import { useAuth } from '@/data/AuthContext';
 import { toURLSearch } from "@/utils/utils";
 
 const tokenizable = token.configure();
 
 export class ArtAPIClass extends ExternalAPI {
-  readonly basePath = process.env.NEXT_PUBLIC_API_ART_URL || 'http://fallback-prod.url'; 
+  readonly basePath = process.env.NEXT_PUBLIC_API_ART_URL || 'http://fallback-prod.url';
 
   //#region RefEmpleadores
   readonly refEmpleadoresURL = () => this.getURL({ path: "/api/Empresas" }).toString();
@@ -22,46 +22,52 @@ export class ArtAPIClass extends ExternalAPI {
   //#endregion
 
   //#region Establecimientos
-  readonly getEstablecimientosURL = (params: { CUIT?: number | string } = {}) => {
-    // params.CUIT ??= useAuth().user?.empresaCUIT ?? 0; // se pasa desde el componente
-    const cuit = params.CUIT ?? 0;
-    return this.getURL({ path: `/api/Establecimientos/Empresa/${cuit}` }).toString();
+  readonly getEstablecimientosURL = (params: ParametersEstablecimientoByCUIT = {}) => {
+    return this.getURL({
+      path: "/api/Establecimientos/Empresa",
+      search: toURLSearch(params),
+    }).toString();
   };
-  getEstablecimientos = async (params: { CUIT?: number | string } = {}) => tokenizable.get(
-    this.getEstablecimientosURL(params),
-  ).then(({ data }) => data);
-  useGetEstablecimientos = (params: { CUIT?: number | string } = {}) => useSWR(
-    [this.getEstablecimientosURL(params), token.getToken()], () => this.getEstablecimientos(params),
+
+  getEstablecimientos = async (params: ParametersEstablecimientoByCUIT = {}) =>
+    tokenizable.get(
+      this.getEstablecimientosURL(params),
+    ).then(({ data }) => data);
+
+  useGetEstablecimientos = (params: ParametersEstablecimientoByCUIT = {}) => useSWR(
+    [this.getEstablecimientosURL(params), token.getToken()],
+    () => this.getEstablecimientos(params),
     {
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
     }
   );
+  //#endregion
 
-  
 
-
-    readonly getEstablecimientoByIdURL = (id: number) => {
-    return this.getURL({ path: `/api/Establecimientos/${id}` }).toString();
+  //#region Establecimiento por Id
+  readonly getEstablecimientoByIdURL = (params: EstablecimientoById) => {
+    return this.getURL({
+      path: `/api/Establecimientos/${params.id}`,
+    }).toString();
   };
-  getEstablecimientoById = async (id: number) => tokenizable.get(
-    this.getEstablecimientoByIdURL(id),
-  ).then(({ data }) => data);
-  useGetEstablecimientoById = (id?: number) => useSWR(
-    id && token.getToken() ? [this.getEstablecimientoByIdURL(id), token.getToken()] : null, () => this.getEstablecimientoById(id as number),
-     {
 
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
+  getEstablecimientoById = async (params: EstablecimientoById) =>
+    tokenizable
+      .get(this.getEstablecimientoByIdURL(params))
+      .then(({ data }) => data);
 
-    }     
-  );
-
-
-  
-
-
-
+  useGetEstablecimientoById = (params?: EstablecimientoById) =>
+    useSWR(
+      params && params.id && token.getToken()
+        ? [this.getEstablecimientoByIdURL(params), token.getToken()]
+        : null,
+      () => this.getEstablecimientoById(params as EstablecimientoById),
+      {
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+      }
+    );
   //#endregion
 
   //#region FormulariosRAR
@@ -74,41 +80,48 @@ export class ArtAPIClass extends ExternalAPI {
   ).then(({ data }) => data);
   useGetFormulariosRARURL = (params: ParametersFormularioRar = {}) => useSWR(
     [this.getFormulariosRARURL(params), token.getToken()], () => this.getFormulariosRAR(params),
-     {
+    {
       // No volver a revalidar al volver al foco, reconectar o al montar si ya hay cache
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
       //revalidateOnMount: false,
       //dedupingInterval: 1000 * 60 * 60, // 1 hora (ajusta si hace falta) // Tiempo en ms durante el cual SWR deduplica solicitudes iguales (evita re-fetch frecuente)
       // Si quieres que la clave no dispare fetch hasta que exista token, puedes usar: (token.getToken() ? key : null)
-    }     
+    }
   );
 
- //  Formulario RAR por interno (/api/FormulariosRAR/{id})
- readonly getFormularioRARByIdURL = (id: number) => this.getURL({ path: `/api/FormulariosRAR/${id}` }).toString();
- getFormularioRARById = async (id: number) => tokenizable.get(
-   this.getFormularioRARByIdURL(id),
- ).then(({ data }) => data);
- useGetFormularioRARById = (id?: number) => useSWR(
-   id && token.getToken() ? [this.getFormularioRARByIdURL(id), token.getToken()] : null,
-   () => this.getFormularioRARById(id as number),
-   {
-     revalidateOnFocus: false,
-     revalidateOnReconnect: false,
-   }
- );
+  //  Formulario RAR por interno (/api/FormulariosRAR/{id})
+  readonly getFormularioRARByIdURL = (interno: FormularioRAR['InternoFormularioRAR']) =>
+    this.getURL({ path: `/api/FormulariosRAR/${interno}` }).toString();
+
+  getFormularioRARById = async (interno: FormularioRAR['InternoFormularioRAR']) =>
+    tokenizable.get(
+      this.getFormularioRARByIdURL(interno),
+    ).then(({ data }) => data);
+
+  useGetFormularioRARById = (interno?: FormularioRAR['InternoFormularioRAR']) => useSWR(
+    interno && token.getToken()
+      ? [this.getFormularioRARByIdURL(interno), token.getToken()]
+      : null,
+    () => this.getFormularioRARById(interno as FormularioRAR['InternoFormularioRAR']),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
   //#endregion
 
-    //#region Empresas por CUIT
-  readonly getEmpresaByCUITURL = (params: { CUIT?: number | string } = {}) => {
+  //#region Empresas por CUIT
+  readonly getEmpresaByCUITURL = (params: ParametersEmpresaByCUIT = {}) => {
     return this.getURL({ path: "/api/Empresas/CUIT", search: toURLSearch(params) }).toString();
   };
 
-  getEmpresaByCUIT = async (params: { CUIT?: number | string } = {}) =>
+  getEmpresaByCUIT = async (params: ParametersEmpresaByCUIT = {}) =>
     tokenizable.get(this.getEmpresaByCUITURL(params))
       .then(({ data }) => data);
 
-  useGetEmpresaByCUIT = (params: { CUIT?: number | string } = {}) => useSWR(
+  useGetEmpresaByCUIT = (params: ParametersEmpresaByCUIT = {}) => useSWR(
     [this.getEmpresaByCUITURL(params), token.getToken()],
     () => this.getEmpresaByCUIT(params),
     {
